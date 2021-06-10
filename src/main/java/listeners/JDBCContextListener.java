177cc770f57e859b6d93406560035db72f9cc2c3
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-06-10 09:11:11 +0300

    fix bugs

--- src/main/java/listeners/JDBCContextListener.java
+++ src/main/java/listeners/JDBCContextListener.java
@@ -24,8 +24,9 @@ public class JDBCContextListener implements ServletContextListener {
         try {
             System.out.println("data source = jdbc/heroku_a6ba2d62a41cee6 ");
             Context initContext = new InitialContext();
-            Context envContext = (Context) initContext.lookup("java:comp/env");
-            DataSource ds = (DataSource) envContext.lookup("jdbc/heroku_a6ba2d62a41cee6");
+//            Context envContext = (Context) initContext.lookup("java:comp/env");
+//            DataSource ds = (DataSource) envContext.lookup("jdbc/heroku_a6ba2d62a41cee6");
+            DataSource ds = (DataSource) initContext.lookup("java:/comp/env/jdbc/heroku_a6ba2d62a41cee6");
             System.out.println("Connection to database: "+ds.toString());
             
             RequestDaoImpl reqService = RequestDaoImpl.getInstance(ds);
